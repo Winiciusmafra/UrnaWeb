520c3bb5d78a654042185063f94fd812d9c00179
Winicius Mafra <winicius.mafra@gmail.com>
2021-03-10 19:38:42 -0300

    candidates and votes model was created

---
+++ UrnaWeb/Models/Candidate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrnaWeb.Models
+{
+    public class Candidate
+    {
+        public int Id { get; set; }
+        public string VicePresident { get; set; }
+        public DateTime DateRegister { get; set; }
+        public int subtitle { get; set; }
+
+        public ICollection<Vote> Votes { get; set; } = new List<Vote>();
+    
+        public Candidate()
+        {
+
+        }
+
+        public Candidate(int id, string vicePresident, DateTime dateRegister, int subtitle)
+        {
+            Id = id;
+            VicePresident = vicePresident;
+            DateRegister = dateRegister;
+            this.subtitle = subtitle;
+        }
+
+        public void AddVotes(Vote v)
+        {
+            Votes.Add(v);
+        }
+
+        //public double TotalVotes(DateTime initial, DateTime final)
+        //{
+        //    return Votes.Sum(vote => vote.TotalVotes(initial, final));
+        //}
+    }
+}
--- UrnaWeb/Models/Vote.cs
+++ UrnaWeb/Models/Vote.cs
@@ -5,7 +5,22 @@ using System.Threading.Tasks;
 
 namespace UrnaWeb.Models
 {
-    public class Votes
+    public class Vote
     {
+        public int Id { get; set; }
+        public DateTime DateRegister { get; set; }
+        public Candidate IdCanditade { get; set; }
+
+
+        public Vote()
+        {
+        }
+
+        public Vote(int id, DateTime dateRegister, Candidate idCanditade)
+        {
+            Id = id;
+            DateRegister = dateRegister;
+            IdCanditade = idCanditade;
+        }
     }
 }
