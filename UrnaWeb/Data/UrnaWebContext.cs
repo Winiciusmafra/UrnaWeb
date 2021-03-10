6decb245bb1ee928a99dee64055aad837143351a
Winicius Mafra <winicius.mafra@gmail.com>
2021-03-10 19:43:10 -0300

    CRUD Scaffolding

---
+++ UrnaWeb/Data/UrnaWebContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UrnaWeb.Models;
+
+namespace UrnaWeb.Data
+{
+    public class UrnaWebContext : DbContext
+    {
+        public UrnaWebContext (DbContextOptions<UrnaWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UrnaWeb.Models.Candidate> Candidate { get; set; }
+    }
+}
