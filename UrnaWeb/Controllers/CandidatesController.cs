019ab2666d376b407a8772e5e2080a0d3cf57932
Winicius Mafra <winicius.mafra@gmail.com>
2021-03-13 19:02:39 -0300

    Votes and Candidates controller

---
+++ UrnaWeb/Controllers/CandidatesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrnaWeb.Controllers
+{
+    public class CandidatesController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
